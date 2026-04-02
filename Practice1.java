c4c09b40d18bccb51877cb8588c1c8f148bc8a7b
zhangtianlu0613-hu <zhangtianlu0613@gmail.com>
2026-04-02 13:11:19 +0900

    2026年4月2日 変数①【提出】

---
+++ Practice1.java
@@ -0,0 +1,12 @@
+package java_ex;
+
+public class Practice1 {
+
+	public static void main(String[] args) {
+
+		int age = 20;
+		String name = "Taro";
+		System.out.println("私は" + name + "です。年齢は" + age + "歳です。");
+	}
+
+}
